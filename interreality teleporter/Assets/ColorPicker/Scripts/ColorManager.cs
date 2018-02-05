d8d2d35d97d0a903ce8d56866e0d844ec7d922a9
buckyslc <martinglaubitz@gmail.com>
2018-02-05 16:39:32 -0700

    colorpicker works in VR
    does not draw on whale though

---
+++ interreality teleporter/Assets/ColorPicker/Scripts/ColorManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorManager : MonoBehaviour {
+
+    public Color color = Color.blue;
+	
+	void OnColorChange(HSBColor color)
+    {
+        this.color = color.ToColor();	
+	}
+}
